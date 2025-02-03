7855d2a0a8f7b407188fecc892334ec319803e53
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-03 19:11:42 +0000

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,22 +1,23 @@
-export const cart = []
+export const cart = [
+  { productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6", quantity: 2 },
+  {productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d', quantity: 1}
+];
 
 export function addToCart(productId) {
-    let matchingProduct;
-  
-    cart.forEach((product) => {
-      if (product.productId === productId) {
-        matchingProduct = product;
-      }
-    });
-  
-    if (matchingProduct) {
-      matchingProduct.quantity++;
-    } else {
-      cart.push({
-        productId,
-        quantity: 1,
-      });
+  let matchingProduct;
+
+  cart.forEach((product) => {
+    if (product.productId === productId) {
+      matchingProduct = product;
     }
+  });
+
+  if (matchingProduct) {
+    matchingProduct.quantity++;
+  } else {
+    cart.push({
+      productId,
+      quantity: 1,
+    });
   }
-
-
+}
