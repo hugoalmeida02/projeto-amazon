913d780624a199e84dbefbaa805a43d30a4430d0
Hugo Almeida <116381112+hugoalmeida02@users.noreply.github.com>
2025-02-08 15:25:33 +0000

    Create class version of Cart

---
+++ data/cart-class.js
@@ -0,0 +1,92 @@
+class Cart {
+  cartItems;
+  localStorageKey;
+
+  constructor(key) {
+    this.localStorageKey = key;
+    this.loadFromStorage();
+  }
+
+  loadFromStorage() {
+    this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
+
+    if (!this.cartItems) {
+      this.cartItems = [
+        {
+          productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+          quantity: 2,
+          deliveryOptionId: "1",
+        },
+        {
+          productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+          quantity: 1,
+          deliveryOptionId: "2",
+        },
+      ];
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+  }
+
+  updateCartQuantity(quantityCart) {
+    let quantity = 0;
+
+    this.cartItems.forEach((product) => {
+      quantity += product.quantity;
+    });
+
+    document.querySelector(".js-quantity").textContent = quantity;
+  }
+
+  addToCart(productId, quantity) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += quantity;
+    } else {
+      this.cartItems.push({
+        productId,
+        quantity: quantity,
+        deliveryOptionId: "1",
+      });
+    }
+    this.saveToStorage();
+  }
+
+  removeFromCart(productId) {
+    const updatedCart = this.cartItems.filter(
+      (item) => item.productId !== productId
+    );
+    this.cartItems = updatedCart;
+    this.saveToStorage();
+  }
+
+  updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    this.cartItems.forEach((cartItem) => {
+      if (productId === cartItem.productId) {
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    this.saveToStorage();
+  }
+}
+
+const cart = new Cart("cart-oop");
+const businessCart = new Cart("businnessCart-oop");
+
+console.log(cart);
+console.log(businessCart);
+console.log(businessCart instanceof Cart);
